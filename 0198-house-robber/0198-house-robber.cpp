8f54ed553cbef8e99198362e4661e9ceb4dc0484
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-04-06 17:20:15 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (86.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        int prev_prev=nums[0], prev=max(nums[0],nums[1]),res=max(nums[0], nums[1]);
-        for(int i=3; i<=n ; i++)
+        int prev2=0, prev=0, cur=0;
+        for(auto i: nums)
         {
-            res=max(prev, prev_prev+nums[i-1]);
-            prev_prev=prev;
-            prev=res;
+            cur= max(prev, prev2+i);
+            prev2=prev;
+            prev=cur;
         }
-        return res;
+        return cur;
     }
 };

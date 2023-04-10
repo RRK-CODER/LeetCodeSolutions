f28b12284f9a245c8b9e551a65c0a65add0ebca0
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-04-10 12:09:52 +0530

    Time: 2 ms (46.33%), Space: 6.4 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.length();
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else if(s[i]==')' || s[i]=='}' || s[i]==']')
+            {
+                if(st.empty()) return false;
+                else if(s[i]==')' && st.top()!='(')
+                    return false;
+                else if(s[i]=='}' && st.top()!='{')
+                    return false;
+                else if(s[i]==']' && st.top()!='[')
+                    return false;
+                else 
+                    st.pop();
+            }
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};

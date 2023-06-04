49b7da567deda0a8aa32395bf5a81e36cf8c525e
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-06-04 22:41:35 +0530

    Time: 332 ms (58.16%) | Memory: 9.7 MB (93.21%) - LeetSync

---
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+bool subsetSumToK(int n, int k, vector<int>& nums)
+{
+    vector<bool> prev(k+1, 0), cur(k+1,0);
+    prev[0]=cur[0]=true;
+    if(nums[0]<=k) prev[nums[0]]=true;
+    for(int ind=1; ind<n; ind++){
+        for(int target=1; target<=k; target++){
+            bool notTake= prev[target];
+            bool take=false;
+        if(nums[ind]<=target) take=prev[target-nums[ind]];
+         cur[target] = take|notTake;
+        }
+        prev=cur;
+    }
+     return prev[k];
+}
+public:
+bool canPartition(vector<int>& nums) {
+        int sum=0, n=nums.size();
+        for(int i=0; i<nums.size(); i++)
+            sum+=nums[i];
+        int tar=sum/2;
+        if(sum%2!=0) return false;
+        return subsetSumToK(n, tar, nums);
+    }
+};

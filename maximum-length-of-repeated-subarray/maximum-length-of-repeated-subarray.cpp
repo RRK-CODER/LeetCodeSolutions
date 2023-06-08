c32e115fb024d792a77906401f5d0f47cf950924
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-06-08 13:57:52 +0530

    Time: 202 ms (79.63%) | Memory: 11.1 MB (90.50%) - LeetSync

--- maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int m=nums1.size(),n=nums2.size(),result=0;
-        int dp[m+1][n+1];
-        for(int i=0; i<=m; i++)
-        dp[i][0];
-        for(int i=0; i<=n; i++)
-            dp[0][i];
-            
-        for(int i=0; i<=m ;i++)
-        {
-            for(int j=0;j<=n; j++)
-            {
+        vector<int> prev(m+1,0), cur(n+1,0);
+        for(int i=0; i<=m ;i++){
+            for(int j=0;j<=n; j++){
                 if(i==0 || j==0)
-                    dp[i][j]=0;
+                    cur[j]=0;
                 else if(nums1[i-1]==nums2[j-1])
-                { dp[i][j]=1+ dp[i-1][j-1];
-                result=max(result,dp[i][j]);}  
+                { cur[j]=1+ prev[j-1];
+                result=max(result,cur[j]);}  
                 else
-                {
-                    dp[i][j]=0;
-                }
-                    
+                    cur[j]=0;     
             }
+            prev=cur;
         }
         return result;
     }

f4b29838fa577f8c071ed93dd659641c7a84d3be
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-06-08 14:01:14 +0530

    Time: 187 ms (27.26%) | Memory: 6.6 MB (94.12%) - LeetSync

---
+++ longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    
+   int lcs(string text1, string text2) {
+        int m= text1.length(), n= text2.length();
+vector<int> cur(m+1,0), prev(n+1,0);
+        for(int i=1; i<= m; i++)
+        {
+            for(int j=1; j<= n ; j++)
+            {
+                if(text1[i-1]== text2[j-1])
+                    cur[j]= 1 + prev[j-1];
+                else
+                {
+                    cur[j]=max(prev[j], cur[j-1]);
+                }
+                    
+            }
+            prev=cur;
+        }
+        return cur[n];
+    }
+    
+public:
+    int longestPalindromeSubseq(string s) {
+        string t=s;
+        reverse(t.begin(), t.end());
+        return lcs(s, t);
+    }
+};

23daeb00d1129db0f67337817a1ced8afe5bfe09
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-06-09 15:52:10 +0530

    Time: 135 ms (82.02%) | Memory: 93.3 MB (36.18%) - LeetSync

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int maxprofit=0;
+        for(int i=1; i<prices.size(); i++)
+        {
+            int cost=prices[i]-mini;
+            maxprofit=max(maxprofit,cost);
+            mini=min(mini, prices[i]);
+        }
+        return maxprofit;
+        
+    }
+};

d17e05b582d22e5412b60902bd035b8fbc44b693
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-07-04 22:32:22 +0530

    Time: 11 ms (46.18%) | Memory: 9.9 MB (32.35%) - LeetSync

---
+++ single-number-ii/single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(auto it: nums)
+            mp[it]++;
+        
+        int ans=0;
+        for(auto it: mp)
+        {
+            if(it.second==1)
+                ans=it.first;
+        }
+    return ans;
+    }
+};

605b60f3209d5d532276ba75e9fc50c6a90e5b95
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-07-12 06:37:09 +0530

    Time: 271 ms (66.29%) | Memory: 104.7 MB (76.18%) - LeetSync

---
+++ shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& A, int K) {
+        int N = A.size();
+    int res = N + 1;
+    vector<long long> B(N + 1, 0);
+    for (int i = 0; i < N; i++)
+        B[i + 1] = B[i] + A[i];
+    deque<int> d;
+    for (int i = 0; i < N + 1; i++) {
+        while (!d.empty() && B[i] - B[d.front()] >= K) {
+            res = min(res, i - d.front());
+            d.pop_front();
+        }
+        while (!d.empty() && B[i] <= B[d.back()])
+            d.pop_back();
+        d.push_back(i);
+    }
+    return (res <= N) ? res : -1;
+}
+};

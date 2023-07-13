4e47cc0d33d5259548061f4aeb65268da420ceaf
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-07-13 11:00:20 +0530

    Time: 83 ms (73.12%) | Memory: 23.9 MB (98.64%) - LeetSync

---
+++ removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        int j=0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '*')
+            j--;
+            else
+            s[j++]=s[i];
+        }
+        return s.substr(0,j);
+    }
+};

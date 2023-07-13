8eaa46719f42589579fce26f9c9fa3c9e05bc9dc
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-07-13 10:55:17 +0530

    Time: 564 ms (14.10%) | Memory: 23.4 MB (91.53%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& g) {
-        int res=0, n=g.size();
-        for(int i=0; i<n; i++){
+        int n=g.size(), res=0;
+        for(int i=0;i<n; i++)
             for(int j=0; j<n; j++){
-                int k=0; 
-                while(k<n && g[i][k] ==g[k][j])
-                    k++;
-                res += k==n;
+                int k=0;
+                while(k<n && g[i][k]==g[k][j])
+                k++;
+                res+= k==n;
             }
-        }
-        return res;
-            }
+            return res;
+        
+    }
 };

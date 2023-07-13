0ae4a6f85d4294b07ccb7d250e98454898acac51
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-07-13 07:48:13 +0530

    Time: 67 ms (5.30%) | Memory: 46.5 MB (5.47%) - LeetSync

--- longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int i=0, j, k=1;
-        for(j=0;j <nums.size(); ++j){
-            if(nums[j] == 0) k--;
-            if(k<0 && nums[i++] == 0) k++;
+        for(j=0; j<nums.size(); j++){
+            if(nums[j]==0)k--;
+            if(k<0 && nums[i++]==0) k++;
         }
         return j-i-1;
     }

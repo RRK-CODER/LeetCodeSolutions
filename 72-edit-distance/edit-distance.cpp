24256c064ffe1d0601e93a34a256be1edce164a0
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-07-20 10:13:47 +0530

    Time: 11 ms (79.96%) | Memory: 6.4 MB (93.29%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minDistance(string s, string t) {
+        int n=s.size(), m=t.size();
+        vector<int> prev(m+1,0), cur(m+1,0);
+        for(int i=1; i<=m; i++) { prev[i]=i; }
+
+        for(int i=1; i<=n; i++)
+        {
+            cur[0]=i;
+            for(int j=1; j<=m; j++)
+            {
+                if(s[i-1]==t[j-1])
+                cur[j]=prev[j-1];
+                else
+                    cur[j]=1+min(cur[j-1], min(prev[j], prev[j-1]));
+            }
+            prev=cur;
+        }
+        return prev[m];
+    }
+};

fb3ab57cbe0d1ef9128b55877f90753a44c85500
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-07-20 12:41:16 +0530

    Time: 469 ms (77.78%) | Memory: 89.7 MB (58.56%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](vector<int>& src, vector<int>& dst){
+        sort(intervals.begin(), intervals.end(), [](vector<int> &src, vector<int> & dst) {
             return src[0]==dst[0]? src[1]<dst[1]:src[0]<dst[0];
         });
-    priority_queue<int> pq;
-    for(int i=0; i<intervals.size(); i++){
-        if(pq.empty() || intervals[i][0] >=pq.top()){
-            pq.push(intervals[i][1]);
-        }else{
-            int cur=pq.top();
-            if(intervals[i][1]<cur){
-                pq.pop();
+        priority_queue<int> pq;
+        for(int i=0; i<intervals.size(); i++){
+            if(pq.empty() || intervals[i][0] >=pq.top()){
                 pq.push(intervals[i][1]);
             }
+            else{
+                int cur=pq.top();
+                if(intervals[i][1]<cur){
+                    pq.pop();
+                    pq.push(intervals[i][1]);
+                }
+            }
         }
-    }
-    return intervals.size()-pq.size();
+        return intervals.size()-pq.size();
     }
 };

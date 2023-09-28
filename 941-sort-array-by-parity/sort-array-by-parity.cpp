a188e258ae08fe81690c6c9513fd234b29f7bf7e
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2023-09-28 08:15:54 +0530

    Time: 11 ms (24.58%) | Memory: 16.7 MB (12.92%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+    
+        int index=0;
+        for (int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2==0){
+    int temp = nums[index];
+            nums[index++]= nums[i];
+            nums[i]=temp;
+            }
+    }
+    return nums;
+    }
+};

6de791720b4345b225fc0be55417f69209b4d772
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2024-04-05 14:25:41 +0530

    Time: 4 ms (42.41%), Space: 7.4 MB (98.61%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     string makeGood(string s) {
-        int sz=0;
-       while (sz != s.size()) {
-        sz = s.size();
-        for (int i = 0; i + 1 < s.size(); ++i)
-            if (abs(s[i] - s[i + 1]) == 32)
-                s = s.substr(0, i) + s.substr(i + 2);
-    }
-    return s;
+        string ans;
+        
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            ans.push_back(s[i]);
+            
+            while(ans.size() && (ans.back()==s[i+1]+32 || ans.back()==s[i+1]-32))
+            {
+                ans.pop_back();
+                i++;
+            }
+        }
+        
+        return ans;
     }
 };

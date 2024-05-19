fbf268a3309669151f2c3f9a7fbcd51654d91e9b
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2024-05-19 16:55:23 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (81.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int prev=0, prev2=0, cur=0;
-        for(auto i: nums){
-            cur=max(prev, prev2+i);
+        for(auto i: nums)
+        {
+            cur = max(prev, prev2+i);
             prev2=prev;
             prev=cur;
         }

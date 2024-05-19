aa8ef8641473551420be4014aa082b7e1cb0caf4
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2024-05-19 16:46:23 +0530

    Time: 0 ms (100.00%), Space: 16.1 MB (96.31%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-      int memo[1001];
-    int minCost(vector<int>& cost , int n)
-    {            
- if(n<0)return 0;
-        else if(memo[n]!=-1)return memo[n];
-else if(n==0 || n==1)return cost[n];
-        else
-            return memo[n]=min(cost[n]+minCost(cost,n-1),cost[n]+minCost(cost,n-2));
-        
-    }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-         memset(memo,-1,sizeof(memo));
-        return min(minCost(cost,n-1),minCost(cost,n-2));
+        int n= cost.size();
+        int first= cost[0], second=cost[1];
+        for(int i=2; i<n; i++)
+        {
+            int curr= cost[i]+ min(first, second);
+            first = second;
+            second = curr;
         }
+        return min(first, second);
+    }
 };

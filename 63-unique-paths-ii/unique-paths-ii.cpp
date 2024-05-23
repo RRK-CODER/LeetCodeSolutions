cdf011e56867be1fc124a12f85d80bef5d6a9ccd
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2024-05-23 14:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (77.06%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& g) {
-        int n=g.size(), m=g[0].size();
-        vector<int> prev(m,0);
+    int uniquePathsWithObstacles(vector<vector<int>>& num) {
+        int n=num.size(), m=num[0].size(), mod=2* 1e9;
+        vector<int> prev(m, 0);
+
         for(int i=0; i<n; i++)
-        { vector<int> cur(m, 0);
+        {
+            vector<int> cur(m, 0);
             for(int j=0; j<m; j++)
             {
-                if(g[i][j]==1) cur[j]=0;
-                else if(i==0 && j==0) cur[j]=1;
-                else 
-                {
+             if(num[i][j]==1)
+                 cur[j]=0;
+             else if(i==0 && j==0) cur[j]=1;
+             else{
                  int up=0, left=0;
-                if(i>0) up=prev[j];
-                if(j>0) left=cur[j-1];
-                cur[j]=up+left;
-                }
+                 if(i>0) up = prev[j];
+                 if(j>0) left= cur[j-1];
+                 cur[j]=(left+up)%mod;
+             }
             }
-         prev =cur;
+            prev=cur;
         }
         return prev[m-1];
+        
     }
 };

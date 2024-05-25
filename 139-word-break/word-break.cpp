4307c60eb46e4f92b577f32954b2359b9eaa64c6
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2024-05-25 16:03:22 +0530

    Time: 15 ms (36.46%) | Memory: 13.6 MB (51.61%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-  bool wordBreak(string s, vector<string>& wordDict) {
-    const int n = s.length();
-    const int maxLength = getMaxLength(wordDict);
-    const unordered_set<string> wordSet{begin(wordDict), end(wordDict)};
-    vector<int> dp(n + 1);
-    dp[0] = true;
-
-    for (int i = 1; i <= n; ++i)
-      for (int j = i - 1; j >= 0; --j) {
-        if (i - j > maxLength)
-          break;
-        if (dp[j] && wordSet.count(s.substr(j, i - j))) {
-          dp[i] = true;
-          break;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n =s.size();
+        
+        set<string> dict;
+        for(auto w: wordDict)
+            dict.insert(w);
+        
+        vector<bool> dp(n+1,false);
+        dp[0] = true;
+    for(int i=0;i<n;i++)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                string curr = s.substr(j,i-j+1); 
+                
+                if(dict.find(curr)!=dict.end())
+                    dp[i+1] = dp[i+1] || dp[j]; 
+                
+                if(dp[i+1])
+                    break;
+            }
         }
-      }
-
-    return dp[n];
-  }
-
- private:
-  int getMaxLength(const vector<string>& wordDict) {
-    return max_element(begin(wordDict), end(wordDict),
-                       [](const auto& a, const auto& b) {
-                         return a.length() < b.length();
-                       })
-        ->length();
-  }
+        
+        return dp[n];
+    }
 };

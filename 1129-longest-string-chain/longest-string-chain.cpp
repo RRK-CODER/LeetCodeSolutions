9280e35c2682800abb3ce047195525406e08792b
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2024-06-12 17:12:19 +0530

    Time: 56 ms (70.56%) | Memory: 16.9 MB (65.01%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkPossible(string &s1, string &s2){
+        if(s1.size() != s2.size() + 1) return false;
+        int first=0, second=0;
+        while(first<s1.size()){
+            if(s1[first]==s2[second] && second<s2.size()){
+                first++;
+                second++;
+            }
+            else{
+                first++;
+            }
+        }
+        if(first == s1.size() && second == s2.size()) return true;
+        return false;
+    }
+    static bool comp(string& s1, string& s2){
+        return s1.size()<s2.size();
+    }
+    int longestStrChain(vector<string>& arr) {
+        int n=arr.size();
+        sort(arr.begin(), arr.end(), comp);
+        vector<int> dp(n, 1),hash(n,1);
+        int maxi=1;
+        for(int i=0; i<n; i++){
+            hash[i]=i;
+            for(int prev=0; prev<i; prev++){
+                if(checkPossible(arr[i], arr[prev]) && 1+ dp[prev]>dp[i]){
+                    dp[i] = 1 + dp[prev];
+                }
+            }
+            if(dp[i] > maxi){
+                maxi=dp[i];
+            }
+        }
+        return maxi;
+    }
+};

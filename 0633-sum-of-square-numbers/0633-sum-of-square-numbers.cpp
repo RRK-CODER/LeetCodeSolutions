7608a7e6d73430b93597f62341bc8d7dbbc0efcf
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2024-06-17 12:30:06 +0530

    Time: 0 ms (100.00%), Space: 7 MB (61.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+    long long low=0, high=sqrt(c);
+        while(low<=high)
+        {
+            if(low*low+high*high==c)
+                return true;
+            else if(low*low+high*high>c)
+                high--;
+            else 
+                low++;
+        }
+        return false;
+    }
+};
